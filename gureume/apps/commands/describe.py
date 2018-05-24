7264698ae8a18f8fdc5463f09fd2e00f7b82c0bf
kalleh <wallbomk@amazon.com>
2018-05-24 15:08:11 +0200

    added open command and watch arg on describe app

--- gureume/apps/commands/describe.py
+++ gureume/apps/commands/describe.py
@@ -1,7 +1,9 @@
 import click
+import click_spinner
 import os
 import requests
 import json
+import time
 
 from gureume.cli import pass_context
 from gureume.lib.util import request, json_to_table
@@ -9,48 +11,70 @@ from gureume.lib.util import request, json_to_table
 
 @click.command('describe', short_help='Displays details about app')
 @click.argument('name')
+@click.option('--watch', is_flag=True, help='Automatically update the stauts every 5s')
 @pass_context
-def cli(ctx, name):
+def cli(ctx, name, watch):
     """Display detailed information about the application."""
     apps = {}
 
     id_token = ctx.config.get('default', 'id_token')
     api_uri = ctx.config.get('default', 'api_uri')
 
-    # Get app status
-    url = api_uri + '/apps/' + name
-    headers = {'Authorization': id_token}
+    # Start a loop that checks for stack creation status
+    with click_spinner.spinner():
+        while True:
+            # Get app status
+            url = api_uri + '/apps/' + name
+            headers = {'Authorization': id_token}
+            
+            r = request('get', url, headers)
+            apps = json.loads(r.text)
 
-    r = request('get', url, headers)
-    apps = json.loads(r.text)
+            # Get CloudFormation Events
+            url = api_uri + '/events/' + name
 
-    # Get CloudFormation Events
-    url = api_uri + '/events/' + name
+            r = request('get', url, headers)
+            events = json.loads(r.text)
 
-    r = request('get', url, headers)
-    events = json.loads(r.text)
+            if watch:
+                click.clear()
 
-    click.secho("=== " + apps['name'], fg='blue')
-    click.secho("Description: " + apps['description'])
+            click.secho("=== " + apps['name'], fg='blue')
+            click.secho("Description: " + apps['description'])
 
-    # print status yellow if in progress, completed is green
-    if(apps['status'].endswith('_IN_PROGRESS')):
-        click.secho("Status: " + apps['status'], fg='yellow')
-    elif(apps['status'].endswith('_COMPLETE')):
-        click.secho("Status: " + apps['status'], fg='green')
-    else:
-        click.secho("Status: " + apps['status'], fg='red')
+            # print status yellow if in progress, completed is green
+            if(apps['status'].endswith('_IN_PROGRESS')):
+                click.secho("Status: " + apps['status'], fg='yellow')
+            elif(apps['status'].endswith('_COMPLETE')):
+                click.secho("Status: " + apps['status'], fg='green')
+            else:
+                click.secho("Status: " + apps['status'], fg='red')
 
-    if 'endpoint' in apps:
-        click.secho("Endpoint: " + apps['endpoint'], fg='green')
-    if 'repository' in apps:
-        click.secho("Repository: " + apps['repository'], fg='green')
-    if 'service_role' in apps:
-        click.secho("Service Role: " + apps['service_role'], fg='green')
+            if 'endpoint' in apps:
+                click.secho("Endpoint: " + apps['endpoint'], fg='green')
+            if 'repository' in apps:
+                click.secho("Repository: " + apps['repository'], fg='green')
+            if 'service_role' in apps:
+                click.secho("Service Role: " + apps['service_role'], fg='green')
 
-    # iterate over and print tags
-    click.secho("Tags: ")
-    for key, val in apps['tags'].items():
-        click.secho("- {}: {}".format(key, val))
+            # iterate over and print tags
+            click.secho("Tags: ")
+            for key, val in apps['tags'].items():
+                click.secho("- {}: {}".format(key, val))
 
-    click.echo(json_to_table(events))
+            click.echo(json_to_table(events))
+            
+            if not watch:
+                break
+            
+            click.echo('Working on: {}'.format(name))
+            click.echo('This usually takes a couple of minutes...')
+            click.echo('This call is asynchrounous so feel free to Ctrl+C ' \
+                        'anytime and it will continue running in background.')
+
+            # Stop loop if task is complete
+            if apps['status'].endswith('_COMPLETE') and not watch:
+                break
+
+            # refresh every 5 seconds
+            time.sleep(5)
