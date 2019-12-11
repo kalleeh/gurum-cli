13f3f65df03d604b2f9def0de41459b621828b6c
kalleh <wallbomk@amazon.com>
2019-12-11 09:23:28 +0100

    fix: remove unused mutating functions

--- gurumcli/commands/services/services.py
+++ gurumcli/commands/services/services.py
@@ -16,12 +16,9 @@ setting up stdin/stdout etc
 
 import click
 
-from .create.cli import cli as create_cli
 from .describe.cli import cli as describe_cli
-from .destroy.cli import cli as destroy_cli
 from .logs.cli import cli as logs_cli
 from .ls.cli import cli as ls_cli
-from .update.cli import cli as update_cli
 
 
 @click.group()
@@ -33,9 +30,6 @@ def cli():
 
 
 # Add individual commands under this group
-cli.add_command(create_cli)
 cli.add_command(describe_cli)
-cli.add_command(destroy_cli)
 cli.add_command(logs_cli)
 cli.add_command(ls_cli)
-cli.add_command(update_cli)
