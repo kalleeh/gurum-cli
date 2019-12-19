6349f9f43bc0e97605045c438e78ecd6884d6649
Karl Wallbom <wallbomk@amazon.com>
2019-12-19 15:42:21 +0100

    refactor: simplify return statement

--- gurumcli/lib/utils/keyring_api.py
+++ gurumcli/lib/utils/keyring_api.py
@@ -24,9 +24,7 @@ def set_github_secret(key, value):
 
 def _get_secret(namespace, key):
     try:
-        secret = keyring.get_password(namespace, key)
-
-        return secret
+        return secret = keyring.get_password(namespace, key)
     except keyring.errors.KeyringError as ex:
         LOGGER.debug(ex)
     except Exception as ex:
