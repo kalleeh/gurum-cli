b383f260df04a4d02fc2f318c135fb28fe614217
Karl Wallbom <wallbomk@amazon.com>
2020-01-08 16:11:03 +0100

    feat: change name from destroy to down

--- gurumcli/commands/down/down_orchestrator.py
+++ gurumcli/commands/down/down_orchestrator.py
@@ -16,42 +16,42 @@ from gurumcommon.logger import configure_logger
 
 LOGGER = configure_logger(__name__)
 
-class DestroyOrchestrator:
+class DownOrchestrator:
 
     def __init__(self, api_client, config, project):
         self.cfg = config
         self.project = project
         self.api_client = api_client
 
-    def destroy_environment(self, environment):
+    def down_environment(self, environment):
         LOGGER.info('Destroying Environment: %s', environment['name'])
         payload = {}
 
         payload['name'] = '{0}-{1}'.format(self.project['name'], environment['name'])
 
         try:
-            self.api_client.delete(resource='apps', payload=json.dumps(payload))
+            self.api_client.apps.delete(payload['name'])
         except Exception:
             raise UnknownError
 
-    def destroy_pipeline(self):
+    def down_pipeline(self):
         LOGGER.info('Destroying %s Pipeline.', format(self.project['source']['provider']))
         payload = {}
 
         payload['name'] = self.project['name']
 
         try:
-            self.api_client.delete(resource='pipelines', payload=json.dumps(payload))
+            self.api_client.pipelines.delete(payload['name'])
         except Exception:
             raise UnknownError
 
-    def destroy_service(self, service):
+    def down_service(self, service):
         LOGGER.info('Destroying Service: %s', service['name'])
         payload = {}
 
         payload['name'] = '{0}-{1}'.format(self.project['name'], service['name'])
 
         try:
-            self.api_client.delete(resource='services', payload=json.dumps(payload))
+            self.api_client.services.delete(payload['name'])
         except Exception:
             raise UnknownError
