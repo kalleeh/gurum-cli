74890f53421354fe0c2dd10b6cd982ac4098d448
Sam Hendriksen <samhendr@amazon.com>
2020-01-21 16:55:07 +0100

    fix: Karl\'s tests

--- gurumcommon/clients/tests/pipeline_actions_test.py
+++ gurumcommon/clients/tests/pipeline_actions_test.py
@@ -3,28 +3,28 @@
 
 # pylint: skip-file
 
-
 from pytest import fixture, raises
-from unittest import mock
+from unittest import mock, TestCase
 import json
 
 from gurumcommon.clients.pipeline_actions import PipelineActions
 from stubs import pipeline_actions_stub 
 
-@fixture
-def cls():
-    return PipelineActions(
-        api_uri='https://test.com',
-        headers={'Authorization': 'IAmAToken'}
-    )
+class TestStringMethods(TestCase):
 
-def test_it_fails_to_get_pipeline_states(cls):
-    with mock.patch('gurumcommon.connection_handler.request', mock.MagicMock(return_value=pipeline_actions_stub.valid_connection_handler_raw_response)) as mock_request:
-        expected_method = 'get'
-        expected_uri = 'https://test.com/pipelines/MyPipeline/states'
-        expected_headers = {'Authorization': 'IAmAToken'}
+    def setUp(self):
+        self.pipeline_actions = PipelineActions(
+            api_uri='https://test.com',
+            headers={'Authorization': 'IAmAToken'}
+        )
 
-        result = cls.get_states('MyPipeline')
+    def test_it_fails_to_get_pipeline_states(self):
+        with mock.patch('gurumcommon.connection_handler.request', mock.MagicMock(return_value=pipeline_actions_stub.valid_connection_handler_raw_response)) as mock_request:
+            expected_method = 'get'
+            expected_uri = 'https://test.com/pipelines/MyPipeline/states'
+            expected_headers = {'Authorization': 'IAmAToken'}
 
-        assert result == pipeline_actions_stub.valid_pipeline_actions_get_states_response
-        # assert mock_request.assert_called_once_with(expected_method, expected_uri, expected_headers)
+            result = self.pipeline_actions.get_states('MyPipeline')
+
+            self.assertEqual(result, pipeline_actions_stub.valid_pipeline_actions_get_states_response)
+            mock_request.assert_called_once_with(expected_method, expected_uri, expected_headers)
