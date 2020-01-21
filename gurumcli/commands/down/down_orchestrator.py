0ae37231bf864740896d8e296659c617cf8b4fc4
Karl Wallbom <wallbomk@amazon.com>
2020-01-21 11:00:23 +0100

    fix: use correct name for gurum down

--- gurumcli/commands/down/down_orchestrator.py
+++ gurumcli/commands/down/down_orchestrator.py
@@ -23,33 +23,28 @@ class DownOrchestrator:
 
     def down_environment(self, environment):
         LOGGER.info('Destroying Environment: %s', environment['name'])
-        payload = {}
 
-        payload['name'] = '{0}-{1}'.format(self.project['name'], environment['name'])
+        name = '{0}-{1}'.format(self.project['name'], environment['name'])
 
         try:
-            self.api_client.apps.delete(payload['name'])
+            self.api_client.apps.delete(name)
         except Exception:
             raise UnknownError
 
     def down_pipeline(self):
         LOGGER.info('Destroying %s Pipeline.', format(self.project['source']['provider']))
-        payload = {}
-
-        payload['name'] = self.project['name']
 
         try:
-            self.api_client.pipelines.delete(payload['name'])
+            self.api_client.pipelines.delete(self.project['name'])
         except Exception:
             raise UnknownError
 
     def down_service(self, service):
         LOGGER.info('Destroying Service: %s', service['name'])
-        payload = {}
 
-        payload['name'] = '{0}-{1}'.format(self.project['name'], service['name'])
+        name = '{0}-{1}'.format(self.project['name'], service['name'])
 
         try:
-            self.api_client.services.delete(payload['name'])
+            self.api_client.services.delete(name)
         except Exception:
             raise UnknownError
